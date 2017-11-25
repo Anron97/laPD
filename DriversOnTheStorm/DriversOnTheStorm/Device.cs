bc932528536cc9460127c262b5c8c049207cdaa3
Andru Shakarov <Anron2012@mail.ru>
2017-11-25 22:50:24 +0300

    Add form and entity.

---
+++ DriversOnTheStorm/DriversOnTheStorm/Device.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriversOnTheStorm
+{
+    class Device
+    {
+        public string Name { get; set; }
+        public string ClassGuid { get; set; }
+        public string HardwareId { get; set; }
+        public string Manufacturer { get; set; }
+        public string Description { get; set; }
+        public string Provider { get; set; }
+        public string SysPath { get; set; }
+        public string DevicePath { get; set; }
+        public bool Status { get; set; }
+
+        public Device(string name, string classGuid, string hardwareId, string manufacturer, string description, string provider, string sysPath, string devicePath, bool status)
+        {
+            Name = name;
+            ClassGuid = classGuid;
+            HardwareId = hardwareId;
+            Manufacturer = manufacturer;
+            Description = description;
+            Provider = provider;
+            SysPath = sysPath;
+            DevicePath = devicePath;
+            Status = status;
+        }
+
+
+    }
+}
---
+++ DriversOnTheStorm/DriversOnTheStorm/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace DriversOnTheStorm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(0, -1);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(372, 474);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(378, -1);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(384, 312);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(630, 412);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Disable";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 474);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
