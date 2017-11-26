a8dfe09eafd6feba9a27eddcffa6d800548b058c
Andru Shakarov <Anron2012@mail.ru>
2017-11-26 21:58:48 +0300

    Add Entity class for wifi network

---
+++ WiFi_Networks/WiFi_Networks/WifiNetwork.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiFi_Networks
+{
+    class WifiNetwork
+    {
+        public string Name { get; set; }
+        public string SignalStrength { get; set; }
+        public string Description { get; set; }
+        public List<string> BssId { get; set; }
+        public bool IsSecured { get; set; }
+        public bool IsConnected { get; set; }
+
+        public WiFiNetwork(string name, string signalStrength, string description, List<string> bssId, bool isSecured, bool isConnected)
+        {
+            Name = name;
+            SignalStrength = signalStrength;
+            Description = description;
+            BssId = bssId;
+            IsSecured = isSecured;
+            IsConnected = isConnected;
+        }
+
+        public bool Connect(string password)
+        {
+            Wifi wifi = new Wifi();
+            AccessPoint accessPoint = wifi.GetAccessPoints().FirstOrDefault(x => x.Name.Equals(Name));
+            if (accessPoint != null)
+            {
+                AuthRequest authRequest = new AuthRequest(accessPoint);
+                authRequest.Password = password;
+                return accessPoint.Connect(authRequest);
+            }
+            return false;
+        }
+
+        public string GetBssIds()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("BssIds: ");
+            builder.Append(String.Join("/r/n", BssId));
+            return builder.ToString();
+        }
+    }
+}
