4f03b1d69e8075b262adeced2fd8c61b0edc14bc
Andru Shakarov <Anron2012@mail.ru>
2017-11-26 21:57:45 +0300

    Create main form

---
+++ WiFi_Networks/WiFi_Networks/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WiFi_Networks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Signal = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Network_name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Signal,
+            this.Network_name});
+            this.listView1.Location = new System.Drawing.Point(10, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(363, 431);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Signal
+            // 
+            this.Signal.DisplayIndex = 0;
+            this.Signal.Text = "Signal";
+            // 
+            // Network_name
+            // 
+            this.Network_name.DisplayIndex = 1;
+            this.Network_name.Text = "Name";
+            this.Network_name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Network_name.Width = 300;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 455);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Signal;
+        private System.Windows.Forms.ColumnHeader Network_name;
+    }
+}
+
